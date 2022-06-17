b1fcc63061a1aaa1e04872bbd5617cb273240017
Paluco <paluco.dev@gmail.com>
2022-06-16 21:46:03 -0400

    component and view

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -76,13 +76,13 @@ export const socialNavigation = [
 const Footer = () => {
     
     return(
-      <footer className="bg-white mt-12">
+      <footer className="bg-c-bg-light mt-12">
         <div className="max-w-7xl mx-auto py-6 px-4 overflow-hidden sm:px-6 lg:px-8">
           <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
             {secondaryNavigation.map((item) => (
               <div key={item.name} className="px-5 py-2">
                 <Link href={item.href}>
-                  <a className="text-sm text-gray-500 hover:text-gray-900">
+                  <a className="text-sm text-white hover:text-gray-400">
                     {item.name}
                   </a>
                 </Link>
@@ -91,13 +91,16 @@ const Footer = () => {
           </nav>
           <div className="mt-8 flex justify-center space-x-6">
             {socialNavigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
-                <span className="sr-only">{item.name}</span>
-                <item.icon className="h-6 w-6" aria-hidden="true" />
-              </a>
+              <Link key={item.name} href={item.href}>
+                <a className="text-white hover:text-gray-400">
+                  <span className="sr-only">{item.name}</span>
+                  <item.icon className="h-6 w-6" aria-hidden="true" />
+                </a>
+              </Link>
+              
             ))}
           </div>
-          <p className="mt-8 text-center text-base text-gray-400">&copy; 2022 Company, LLC. All rights reserved.</p>
+          <p className="mt-8 text-center text-base text-gray-400">&copy; 2022 0xPaluCo - All rights reserved.</p>
         </div>
       </footer>
     )
