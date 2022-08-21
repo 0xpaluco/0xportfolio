133eaf4617b25c4c9aa2db4bc5b2016e72785ce8
Paluco <paluco.dev@gmail.com>
2022-08-21 00:19:12 -0400

    types

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -13,3 +13,38 @@ type NextPageWithLayout = NextPage & {
 type AppPropsWithLayout = AppProps & {
     Component: NextPageWithLayout
 }
+
+type ERC20Token = {
+    value: string;
+    token: {
+        contractAddress: string;
+        chain: string | number;
+        decimals: number;
+        name: string;
+        symbol: string;
+        logo?: string | null ;
+        logoHash?: string | null ;
+        thumbnail?: string | null ;
+    }
+}
+
+export interface EvmNFT {
+    token_address:       string;
+    token_id:            string;
+    amount:              string;
+    owner_of:            string;
+    token_hash:          string;
+    block_number_minted: string;
+    block_number:        string;
+    contract_type:       string;
+    name:                string;
+    symbol:              string;
+    token_uri:           string;
+    metadata:            metadata;
+    last_token_uri_sync: Date;
+    last_metadata_sync:  Date;
+}
+
+interface metadata {
+    [key:string]: string
+}
