0d5c7cf1f175a74d61b3e40e22c8b7ac706b3b43
Paluco <paluco.dev@gmail.com>
2022-08-21 00:30:51 -0400

    dao page

--- pages/dao/index.tsx
+++ pages/dao/index.tsx
@@ -1,37 +1,14 @@
-import { ReactElement, ReactNode, useState } from 'react'
-import { GetServerSideProps } from "next";
+import { ReactElement } from 'react'
 import { NextPageWithLayout } from '@/types'
-import { FeedView, HomeView, DaoView } from '@views/index'
+import { DaoView } from '@views/index'
 import WithLayout, { Explore, Simple, Home } from 'src/layouts'
-import { DUMMY_POSTS } from 'lib/dummy'
-import { feedTabs } from '@helpers/routes'
+
 import { Aside } from '@components/index'
-import { catchPromise } from '@helpers/utils';
 
 
-const LIMIT = 1;
+interface PageProps {}
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-
-  const [data, error] = [[], null] //await catchErrors(listPostForFeed());
-  if(error){
-    console.log(error);
-  }
-
-  console.log(data);
-  return {
-    props: {
-      posts: data,
-    } // will be passed to the page component as props
-  }
-}
-
-interface PageProps {
-  posts: any[]
-  //children?: ReactNode;
-}
-
-const DaoPage: NextPageWithLayout = ({ posts }: any) => {
+const DaoPage: NextPageWithLayout = () => {
   return <DaoView></DaoView>
 }
 
