d7c73acb405dec652258118366ef57babbffee09
Paluco <paluco.dev@gmail.com>
2022-08-21 00:19:40 -0400

    auth and sessiom

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,96 @@
+import { ApiRequest, ApiResponse } from "@/types"
+import { getUser } from "@helpers/auth"
+import Moralis from "moralis"
+import NextAuth from "next-auth"
+import { Provider } from "next-auth/providers"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { getCsrfToken } from "next-auth/react"
+import { SiweMessage } from "siwe"
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export default async function auth(req: ApiRequest, res: ApiResponse) {
+  const providers: Provider[] = [
+    CredentialsProvider({
+      name: "Ethereum",
+      credentials: {
+        message: {
+          label: "Message",
+          type: "text",
+          placeholder: "0x0",
+        },
+        signature: {
+          label: "Signature",
+          type: "text",
+          placeholder: "0x0",
+        },
+      },
+      async authorize(credentials) {
+        try {
+          const siwe = new SiweMessage(JSON.parse(credentials?.message || "{}"))
+
+          const nextAuthUrl =
+            process.env.NEXTAUTH_URL ||
+            (process.env.VERCEL_URL
+              ? `https://${process.env.VERCEL_URL}`
+              : null)
+          if (!nextAuthUrl) {
+            return null
+          }
+
+          const nextAuthHost = new URL(nextAuthUrl).host
+          if (siwe.domain !== nextAuthHost) {
+            return null
+          }
+
+          if (siwe.nonce !== (await getCsrfToken({ req }))) {
+            return null
+          }
+
+          await siwe.validate(credentials?.signature || "")
+
+          // Moralis Auth
+          // const user = await getUser(siwe.toMessage(), credentials?.signature!)          
+          
+          return {
+            id: siwe.address
+          }
+        } catch (e) {
+          return null
+        }
+      },
+    }),
+  ]
+
+  const isDefaultSigninPage =
+    req.method === "GET" && req?.query?.nextauth?.includes("signin")
+
+  // Hide Sign-In with Ethereum from default sign page
+  if (isDefaultSigninPage) {
+    providers.pop()
+  }
+
+  return await NextAuth(req, res, {
+    // https://next-auth.js.org/configuration/providers/oauth
+    providers,
+    pages: {
+      signIn: "/",
+    },
+    session: {
+      strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+      async jwt({ token, user }) {
+        user && (token.user = user);
+        return token;
+      },
+      async session({ session, token }) {
+        session.address = token.sub
+        session.user.name = token.sub
+        session.user.image = 'https://www.fillmurray.com/128/128'
+        return session
+      },
+    },
+  })
+}
