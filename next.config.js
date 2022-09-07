4c80092ed373e5b3d295be557b06da683360d3b3
Paluco <paluco.dev@gmail.com>
2022-09-07 00:58:41 -0400

    config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['*', "", 'tailwindui.com', "cdn.discordapp.com", "gateway.ipfs.io"],
+    domains: ['tailwindui.com', "cdn.discordapp.com", "gateway.ipfs.io", "localhost:5000"],
   },
 }
 
