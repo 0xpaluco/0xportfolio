e6fdd4ddfe96fe38c121110fa4d1536121c348d4
Paluco <paluco.dev@gmail.com>
2022-10-15 01:11:04 -0400

    experimental

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: { images: { allowFutureImage: true } },
   images: {
     domains: ['tailwindui.com', "cdn.discordapp.com", "gateway.ipfs.io", "localhost:5000"],
   },
