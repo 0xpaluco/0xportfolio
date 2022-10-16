43f0cfb4c44aa0173ff0a07d51dd25649bab6f7d
Paluco <paluco.dev@gmail.com>
2022-10-15 21:26:59 -0400

    format document

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,38 +7,38 @@ export interface ISession extends Session { }
 const provider = CredentialsProvider({
   name: 'MoralisAuth',
   credentials: {
-      message: {
-          label: 'Message',
-          type: 'text',
-          placeholder: '0x0',
-      },
-      signature: {
-          label: 'Signature',
-          type: 'text',
-          placeholder: '0x0',
-      },
-  },
-    async authorize(credentials) {
-      try {
-        // "message" and "signature" are needed for authorization
-        // we described them in "credentials" above
-        const message = credentials?.message;
-        const signature = credentials?.signature;
-
-        await Moralis.start({ apiKey: process.env.MORALIS_API_KEY });
-
-        const { address, profileId } = (
-          await Moralis.Auth.verify({ message: message!, signature: signature!, network: 'evm' })
-        ).raw;
-
-        const user = { address, profileId, signature };
-        // returning the user object and creating  a session
-        return user;
-      } catch (e) {
-        console.error(e);
-        return null;
-      }
+    message: {
+      label: 'Message',
+      type: 'text',
+      placeholder: '0x0',
     },
+    signature: {
+      label: 'Signature',
+      type: 'text',
+      placeholder: '0x0',
+    },
+  },
+  async authorize(credentials) {
+    try {
+      // "message" and "signature" are needed for authorization
+      // we described them in "credentials" above
+      const message = credentials?.message;
+      const signature = credentials?.signature;
+
+      await Moralis.start({ apiKey: process.env.MORALIS_API_KEY });
+
+      const { address, profileId } = (
+        await Moralis.Auth.verify({ message: message!, signature: signature!, network: 'evm' })
+      ).raw;
+
+      const user = { address, profileId, signature };
+      // returning the user object and creating  a session
+      return user;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  },
 })
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
