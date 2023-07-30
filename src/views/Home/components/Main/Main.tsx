a5bd1f0924874e76114c79a9ad34c4182150cecf
0xpaluco <hello@0xpalu.co>
2023-07-30 15:10:52 -0400

    space added in h1

--- src/views/Home/components/Main/Main.tsx
+++ src/views/Home/components/Main/Main.tsx
@@ -24,7 +24,7 @@ const Main = () => {
                 </Link>
 
                 <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
-                  <span className="block">Hi, I’m Paluco</span>
+                  <span className="block">Hi, I’m Paluco</span>{' '}
                   <span className="block text-c-l-primary">I build things for the future of the web.</span>
                 </h1>
                 <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
