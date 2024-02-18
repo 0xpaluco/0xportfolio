508c8525a7943779d496e8e7a56ec3c516ebb7ff
0xpaluco <hello@0xpalu.co>
2024-02-18 19:58:20 -0400

    added social links to footer

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -3,18 +3,20 @@
 import React from 'react'
 
 import { MainNav, Footer } from '@components/shared'
+import { SocialLinkType } from '@/sanity/lib/queries'
 
 interface Props {
   children: React.ReactNode
   className?: string
+  links: SocialLinkType[]
 }
 
-const MainLayout = ({ children, className }: Props): JSX.Element => {
+const MainLayout = ({ children, links, className }: Props): JSX.Element => {
   return (
     <div className="relative bg-c-bg-light h-screen overflow-auto">
       <MainNav className={className} />
       {children}
-      <Footer links={[]} />
+      <Footer links={links} />
     </div>
   )
 }
